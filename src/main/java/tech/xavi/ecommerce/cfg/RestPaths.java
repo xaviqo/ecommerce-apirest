000eafdf35e6f0a08364637d43fd229a496d3694
Xavi Q <xavi@customer.mdrdesp1.pop.starlinkisp.net>
2025-07-10 16:46:03 +0200

    create pricecontroller to retrieve product price for given date

---
+++ src/main/java/tech/xavi/ecommerce/cfg/RestPaths.java
@@ -0,0 +1,6 @@
+package tech.xavi.ecommerce.cfg;
+
+public class RestPaths {
+    private static final String BASE_API = "/api/v1";
+    public static final String PRICE = BASE_API + "/prices";
+}
---
+++ src/main/java/tech/xavi/ecommerce/rest/PriceController.java
@@ -0,0 +1,32 @@
+package tech.xavi.ecommerce.rest;
+
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import tech.xavi.ecommerce.cfg.RestPaths;
+import tech.xavi.ecommerce.dto.PriceResponse;
+import tech.xavi.ecommerce.service.PriceService;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(RestPaths.PRICE)
+public class PriceController {
+
+    private final PriceService priceService;
+
+    @GetMapping
+    public ResponseEntity<PriceResponse> getPriceForDate(
+            @RequestParam @Positive long brandId,
+            @RequestParam @Positive long productId,
+            @RequestParam @Positive long targetDate
+    ) {
+        return priceService
+                .getPriceForDate(brandId, productId, targetDate)
+                .map(ResponseEntity::ok)
+                .orElseThrow();
+    }
+}
